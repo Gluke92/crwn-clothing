b4a864f292c17411027bc92e6b80a272fcfe9b1b
George Luke <george.luke92@gmail.com>
2019-09-12 14:34:04 -0500

    removing redux logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,6 +11,9 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger]; /* you may want to modify middlwares at some point */
 
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export const persistor = persistStore(store);
